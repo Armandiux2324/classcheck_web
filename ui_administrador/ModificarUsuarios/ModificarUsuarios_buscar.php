ab5a1da61b1f73d6eb6138db6c72e43adcecaa35
Armando <armandocandelasalvarado@gmail.com>
2024-08-10 21:33:32 -0600

    rutas de estilos acomodadas

--- ui_administrador/ModificarUsuarios/ModificarUsuarios_buscar.php
+++ ui_administrador/ModificarUsuarios/ModificarUsuarios_buscar.php
@@ -6,6 +6,7 @@
     <title>ClassCheck - Búsqueda de Usuarios</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../../css/index_style.css">
+    <link rel="stylesheet" href="../../css/main_style.css">
     <script src="../../scripts/index_script.js"></script>
     <script src="../../scripts/main_script.js"></script>
     <script src="../../scripts/admin_script.js"></script>
